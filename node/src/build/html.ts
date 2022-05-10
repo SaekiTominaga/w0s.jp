4a3b6802d36e0361b94d512e1743f540b2299acb
SaekiTominaga <kcsu3470+github@gmail.com>
2022-05-10 13:56:26 +0900

    fix #75 定数を config ファイルから読み込むように変更、その他全体的に見直し

--- node/src/build/html.ts
+++ node/src/build/html.ts
@@ -14,6 +14,12 @@ import PosthtmlMatchClass from '@saekitominaga/posthtml-match-class';
 import posthtmlTimeJapaneseDate from 'posthtml-time-japanese-date';
 import prettier from 'prettier';
 import { JSDOM } from 'jsdom';
+import { NoName as Configure } from '../../configure/type/build';
+import { W0SJp as ConfigureCommon } from '../../configure/type/common';
+
+/* 設定ファイル読み込み */
+const configCommon = <ConfigureCommon>JSON.parse(fs.readFileSync('node/configure/common.json', 'utf8'));
+const config = <Configure>JSON.parse(fs.readFileSync('node/configure/build.json', 'utf8'));
 
 const filesPath = process.argv[2];
 if (filesPath === undefined) {
@@ -25,14 +31,14 @@ fileList.include(filesPath);
 
 fileList.map(async (filePath) => {
 	/* ファイル読み込み */
-	const fileData = fs.readFileSync(filePath).toString();
+	const fileData = (await fs.promises.readFile(filePath)).toString();
 
 	const filePathUrl = filePath.replace(/\\/g, '/');
 	const fileUrl = filePathUrl.substring(filePathUrl.indexOf('/'));
 	const fileName = path.basename(filePath);
 
 	let pagePath = ''; // ページのルート相対パス
-	if (fileName === 'index.html') {
+	if (configCommon.static.indexes?.includes(fileName)) {
 		/* ファイル名が index.html の場合は省略する */
 		pagePath = path.dirname(fileUrl);
 		if (pagePath !== '/') {
@@ -47,15 +53,14 @@ fileList.map(async (filePath) => {
 	const document = new JSDOM(fileData).window.document;
 
 	/* HTML から必要なデータを取得 */
-	const pageTitle = document.querySelector('title')?.textContent?.trim() ?? ''; // ページタイトル
+	const pageTitle = document.querySelector('title')?.textContent ?? ''; // ページタイトル
 
-	const pageDescription =
-		document
-			.querySelector('[itemprop="description"]')
-			?.textContent?.trim()
-			.split('\n')
-			.map((value) => value.trim())
-			.join(' ') ?? undefined; // description
+	const pageDescription = document
+		.querySelector('[itemprop="description"]')
+		?.textContent?.trim()
+		.split('\n')
+		.map((value) => value.trim())
+		.join(' '); // Description
 
 	/* EJS を解釈 */
 	let html = await ejs.renderFile(
@@ -68,7 +73,7 @@ fileList.map(async (filePath) => {
 			},
 		},
 		{
-			views: ['views'],
+			views: [configCommon.views],
 		}
 	);
 
@@ -79,76 +84,45 @@ fileList.map(async (filePath) => {
 		await posthtml([
 			/* リンクアンカーにドメイン情報を付与 */
 			posthtmlAnchorHost({
-				class: 'htmlbuild-domain',
-				host_element: 'b',
-				host_class: 'c-domain',
-				host_parentheses_before: '(',
-				host_parentheses_after: ')',
+				class: config.html.anchor_host.class,
+				host_element: config.html.anchor_host.host_element,
+				host_class: config.html.anchor_host.host_class,
+				host_parentheses_before: config.html.anchor_host.host_parentheses_before,
+				host_parentheses_after: config.html.anchor_host.host_parentheses_after,
 			}),
 
 			/* リンクアンカーにアイコンを付与 */
 			posthtmlAnchorIcon({
-				class: 'htmlbuild-icon',
-				host_info: [
-					{
-						host: 'github.com',
-						site_name: 'GitHub',
-						icon_src: '/assets/image/icon/github.svg',
-					},
-					{
-						host: 'twitter.com',
-						site_name: 'Twitter',
-						icon_src: '/assets/image/icon/twitter.svg',
-					},
-					{
-						host: 'www.youtube.com',
-						site_name: 'YouTube',
-						icon_src: '/assets/image/icon/youtube.svg',
-					},
-					{
-						host: 'www.amazon.co.jp',
-						site_name: 'Amazon',
-						icon_src: '/assets/image/icon/amazon.png',
-					},
-					{
-						host: 'iss.ndl.go.jp',
-						site_name: '国立国会図書館サーチ',
-						icon_src: '/assets/image/icon/ndl.png',
-					},
-					{
-						host: 'www.nicovideo.jp',
-						site_name: 'ニコニコ動画',
-						icon_src: '/assets/image/icon/nicovideo.png',
-					},
-				],
-				icon_class: 'c-link-icon',
-				icon_size: 16,
-				icon_parentheses_before: '(',
-				icon_parentheses_after: ')',
+				class: config.html.anchor_icon.class,
+				host_info: config.html.anchor_icon.host_info,
+				icon_class: config.html.anchor_icon.icon_class,
+				icon_size: config.html.anchor_icon.icon_size,
+				icon_parentheses_before: config.html.anchor_icon.icon_parentheses_before,
+				icon_parentheses_after: config.html.anchor_icon.icon_parentheses_after,
 			}),
 
 			/* Amazon 商品ページのリンクにアソシエイトタグを追加 */
 			posthtmlAnchorAmazonAssociate({
-				class: 'htmlbuild-amazon-associate',
-				associate_id: 'w0s.jp-22',
+				class: config.html.anchor_amazon_associate.class,
+				associate_id: configCommon.paapi.request.partner_tag,
 			}),
 
 			/* 日付文字列を <time datetime> 要素に変換 */
-			posthtmlTimeJapaneseDate({ element: 'span', class: 'htmlbuild-datetime' }),
+			posthtmlTimeJapaneseDate({ element: config.html.time.element, class: config.html.time.class }),
 
 			/* <picture> 要素を使って複数フォーマットの画像を提供する */
-			posthtmlImage({ class: 'htmlbuild-image' }),
+			posthtmlImage({ class: config.html.image.class }),
 
 			/* highlight.js */
 			(tree: posthtml.Node): posthtml.Node => {
 				hljs.registerLanguage('xml', hljsXml);
 				hljs.registerLanguage('javascript', hljsJavaScript);
 				hljs.configure({
-					classPrefix: 'c-code-highlight -',
+					classPrefix: config.html.highlight.htmlbuild_class_prefix,
 				});
 
 				const targetElementInfo = {
-					class: 'htmlbuild-highlight',
+					class: config.html.highlight.class,
 				};
 
 				tree.match({ tag: 'code' }, (node: posthtml.Node) => {
@@ -211,13 +185,13 @@ fileList.map(async (filePath) => {
 			parser: 'html',
 		});
 
-		console.info(`フォーマット処理（prettier）完了: ${filePath}`);
+		console.info(`Prettier finished: ${filePath}`);
 	} catch (e) {
-		console.error(`フォーマット処理（prettier）でエラー: ${filePath}`, e);
+		console.error(`Prettier error: ${filePath}`, e);
 	}
 
 	/* 出力 */
-	const formatHTMLPath = `public/${filePath.substring(filePath.replace(/\\/g, '/').indexOf('/') + 1)}`;
-	await fs.promises.writeFile(formatHTMLPath, htmlFormatted);
-	console.info(`HTML ファイル出力: ${formatHTMLPath}`);
+	const distPath = `${configCommon.static.root}/${filePath.substring(filePath.replace(/\\/g, '/').indexOf('/') + 1)}`;
+	await fs.promises.writeFile(distPath, htmlFormatted);
+	console.info(`HTML file created: ${distPath}`);
 });
