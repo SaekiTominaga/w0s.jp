6e1fbcdaace4858cb74e124f208ece53f8517f09
SaekiTominaga <kcsu3470+github@gmail.com>
2022-07-25 12:02:25 +0900

    fix #127 `<img>` 要素の `width`, `height` 属性値が正しいかチェックする機能を作成

---
+++ node/src/build/html-image-size.ts
@@ -0,0 +1,81 @@
+import fs from 'fs';
+import sizeOf from 'image-size';
+import { globby } from 'globby';
+import { JSDOM } from 'jsdom';
+
+const filesPath = process.argv[2];
+if (filesPath === undefined) {
+	throw new Error('Missing parameter');
+}
+
+(async () => {
+	const fileList = await globby(filesPath);
+
+	const fetchedHost = new Set<string>();
+
+	fileList.forEach(async (filePath) => {
+		/* ファイル読み込み */
+		console.info(filePath);
+		const fileData = (await fs.promises.readFile(filePath)).toString();
+
+		const document = new JSDOM(fileData).window.document;
+
+		for (const imageElement of <NodeListOf<HTMLImageElement>>document.querySelectorAll('img[src^="https://"]')) {
+			const imageUrl = new URL(imageElement.src);
+			if (imageUrl.host === 'media.w0s.jp') {
+				continue;
+			}
+
+			if (fetchedHost.has(imageUrl.host)) {
+				/* 同一ホストへのアクセスは一定間隔を空ける */
+				await new Promise((resolve) => setTimeout(resolve, 1000));
+			} else {
+				fetchedHost.add(imageUrl.host);
+			}
+
+			const widthAttributeValue = imageElement.width;
+			const heightAttributeValue = imageElement.height;
+			if (widthAttributeValue === 0 || heightAttributeValue === 0) {
+				console.warn('<img> 要素に width 属性または height 属性が指定されていない', filePath, imageUrl.toString());
+				continue;
+			}
+
+			console.info('fetch', imageUrl.toString());
+			let response: Response;
+			try {
+				response = await fetch(imageUrl);
+				if (!response.ok) {
+					console.error('Fetch error', imageUrl);
+					continue;
+				}
+			} catch (e) {
+				console.error(e);
+				continue;
+			}
+
+			const arrayBuffer = await response.arrayBuffer();
+			const buffer = Buffer.alloc(arrayBuffer.byteLength);
+			const unit8Array = new Uint8Array(arrayBuffer);
+			buffer.forEach((_, index) => {
+				const v = unit8Array[index];
+				if (v !== undefined) {
+					buffer[index] = v;
+				}
+			});
+
+			const size = sizeOf(buffer);
+			const width = size.width;
+			const height = size.height;
+			if (width === undefined || height === undefined) {
+				console.warn('<img> 要素で指定された画像のサイズが取得できない', filePath, imageUrl.toString());
+				continue;
+			}
+			if (width !== widthAttributeValue) {
+				console.warn(`<img> 要素の width 属性値<${widthAttributeValue}>が実際の値<${width}>と違う`, filePath, imageUrl.toString());
+			}
+			if (height !== heightAttributeValue) {
+				console.warn(`<img> 要素の height 属性値<${heightAttributeValue}>が実際の値<${height}>と違う`, filePath, imageUrl.toString());
+			}
+		}
+	});
+})();
