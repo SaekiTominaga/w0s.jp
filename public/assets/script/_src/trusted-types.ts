52bd6eeca2f45ccbc7f1d5ab04852ffe7411cd58
SaekiTominaga <kcsu3470+github@gmail.com>
2022-09-28 01:10:11 +0900

    fix #169 Trusted Types エラーの console 出力、ScriptURL 許可リストの Origin 対応、許可リストの精査

--- public/assets/script/_src/trusted-types.ts
+++ public/assets/script/_src/trusted-types.ts
@@ -1,23 +1,27 @@
+/**
+ * Trusted Types
+ */
 if (window.trustedTypes !== undefined) {
 	window.trustedTypes.createPolicy('default', {
-		createHTML: (inputText: string): string => inputText,
+		createHTML: (inputText: string): string => {
+			return inputText;
+		},
 		createURL: (inputUrl: string): string => {
 			if (new URL(inputUrl).origin !== new URL(location.href).origin) {
-				throw new Error(`[Trusted URL] URL not accepted: ${inputUrl}`);
+				console.error(`[Trusted URL] URL with different origin are not allowed: ${inputUrl}`);
+				throw new TypeError(`URL with different origin are not allowed: ${inputUrl}`);
 			}
 
 			return inputUrl;
 		},
 		createScriptURL: (inputUrl: string): string => {
-			const WHITELIST = [
-				'/firebase-messaging-sw.js',
-				'https://www.google-analytics.com/analytics.js',
-				'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-			];
-			const WHITELIST_REGEXP = [/^https:\/\/platform.twitter.com\/js\/horizon_tweet\.[0-9a-z]+\.js$/];
+			const ALLOW_URLS: string[] = ['https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'];
+			const ALLOW_ORIGINS: string[] = [];
+			const ALLOW_REGEXPS: RegExp[] = [];
 
-			if (!WHITELIST.includes(inputUrl) && !WHITELIST_REGEXP.some((url: RegExp) => url.test(inputUrl))) {
-				throw new Error(`[Trusted ScriptURL] URL not accepted: ${inputUrl}`);
+			if (!ALLOW_URLS.includes(inputUrl) && !ALLOW_ORIGINS.includes(new URL(inputUrl).origin) && !ALLOW_REGEXPS.some((allow) => allow.test(inputUrl))) {
+				console.error(`[Trusted ScriptURL] URL not accepted: ${inputUrl}`);
+				throw new TypeError(`URL not accepted: ${inputUrl}`);
 			}
 
 			return inputUrl;
