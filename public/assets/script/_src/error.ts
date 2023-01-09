f9b1ecbd1da94a5d69a2fcea8856a5b0c31c4110
SaekiTominaga <kcsu3470+github@gmail.com>
2023-01-09 12:26:08 +0900

    `error.ts` の全体的な調整
    処理自体は変わらない

--- public/assets/script/_src/error.ts
+++ public/assets/script/_src/error.ts
@@ -1,63 +1,62 @@
 import ClosestHTMLPage from '@saekitominaga/closest-html-page';
-import Portal from '@saekitominaga/customelements-portal';
+import PortalAnimation from '@saekitominaga/customelements-portal';
 import ReportJsError from '@saekitominaga/report-js-error';
 import ReportSameReferrer from '@saekitominaga/report-same-referrer';
 
 /**
  * 403, 404, 410 ページ
  */
-const reportJsError = new ReportJsError('https://report.w0s.jp/js', {
-	fetchParam: {
-		location: 'location',
-		message: 'message',
-		filename: 'filename',
-		lineno: 'lineno',
-		colno: 'colno',
-	},
-	fetchContentType: 'application/json',
-	allowFilenames: [/^https:\/\/w0s\.jp\/assets\/script\/.+\.m?js$/],
-	denyUAs: [/Googlebot\/2.1;/],
-});
-reportJsError.init();
-
 const { portalHost } = window;
-if (portalHost === null || portalHost === undefined /* 未対応ブラウザ対策 */) {
-	const reportSameReferrer = new ReportSameReferrer('https://report.w0s.jp/referrer', {
+if (portalHost === null || portalHost === undefined /* <potal> 未対応ブラウザは undefined になる */) {
+	/* JS エラーレポート */
+	new ReportJsError('https://report.w0s.jp/js', {
+		fetchParam: {
+			location: 'location',
+			message: 'message',
+			filename: 'filename',
+			lineno: 'lineno',
+			colno: 'colno',
+		},
+		fetchContentType: 'application/json',
+		allowFilenames: [/^https:\/\/w0s\.jp\/assets\/script\/.+\.m?js$/],
+		denyUAs: [/Googlebot\/2.1;/],
+	}).init();
+
+	/* リファラーレポート */
+	new ReportSameReferrer('https://report.w0s.jp/referrer', {
 		fetchParam: {
 			location: 'location',
 			referrer: 'referrer',
 		},
 		fetchContentType: 'application/json',
 		same: ['https://blog.w0s.jp'],
-	});
-	reportSameReferrer.init();
+	}).init();
 
-	const supportPortalElement = window.HTMLPortalElement !== undefined; // <potal> 要素をサポートしているか
-
-	if (supportPortalElement && window.customElements !== undefined) {
-		customElements.define('w0s-portal', Portal);
-	}
-
+	/* 祖先ページの埋め込み */
 	const closestHTMLPage = new ClosestHTMLPage(['text/html', 'application/xhtml+xml'], 6);
 
-	window.addEventListener('load', async () => {
-		const parentpageMessageElement = <HTMLElement>document.getElementById('parentpage-msg');
-		const parentpageAnchorElement = <HTMLAnchorElement>document.getElementById('parentpage-anchor');
-		const parentpagePortalElement = <HTMLPortalElement>document.getElementById('parentpage-portal');
+	await closestHTMLPage.fetch();
+	const url = closestHTMLPage.getUrl();
+	const title = closestHTMLPage.getTitle();
 
-		await closestHTMLPage.fetch();
-		const url = closestHTMLPage.getUrl();
-		const title = closestHTMLPage.getTitle();
+	if (url !== null && title !== null) {
+		const messageElement = <HTMLElement | null>document.getElementById('parentpage-msg');
+		const anchorElement = <HTMLAnchorElement | null>document.getElementById('parentpage-anchor');
+		const portalElement = <HTMLPortalElement | null>document.getElementById('parentpage-portal');
 
-		if (url !== null && title !== null) {
-			parentpageAnchorElement.href = url;
-			parentpageAnchorElement.textContent = title;
-			parentpageMessageElement.hidden = false;
+		if (messageElement !== null && anchorElement !== null) {
+			messageElement.hidden = false;
 
-			if (supportPortalElement) {
-				parentpagePortalElement.src = url;
-				parentpagePortalElement.title = title;
+			anchorElement.href = url;
+			anchorElement.textContent = title;
+
+			if (portalElement !== null && window.HTMLPortalElement !== undefined /* <potal> 要素をサポートしているか */ && window.customElements !== undefined) {
+				portalElement.src = url;
+				portalElement.title = title;
+				portalElement.hidden = false;
+
+				customElements.define('w0s-portal', PortalAnimation);
 			}
 		}
-	});
+	}
 }
