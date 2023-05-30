0f235c0c4e305453b54c15f2337e551544cc0537
SaekiTominaga <kcsu3470+github@gmail.com>
2023-05-30 13:43:42 +0900

    変数名変更

--- packages/frontend/node/src/BuildComponent.ts
+++ packages/frontend/node/src/BuildComponent.ts
@@ -1,8 +1,8 @@
 import fs from 'node:fs';
-import { NoName as ConfigureBuild } from '../../configure/type/build.js';
+import { NoName as Configure } from '../../configure/type/build.js';
 
 export default class BuildComponent {
-	protected readonly config: ConfigureBuild; // Configure
+	protected readonly config: Configure; // Configure
 
 	constructor() {
 		this.config = JSON.parse(fs.readFileSync('configure/build.json', 'utf8'));
