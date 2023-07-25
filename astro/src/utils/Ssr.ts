35c7cc9b47127479e6c70fe80633b2735787b06a
SaekiTominaga <kcsu3470+github@gmail.com>
2023-07-25 19:52:02 +0900

    SSRページの Content-Type に charset を設定

--- astro/src/utils/Ssr.ts
+++ astro/src/utils/Ssr.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import type { AstroGlobal } from 'astro';
 import Log4js from 'log4js';
 import log4jsConfigure from '../../log4js.config.json';
 import configure from '../../../configure/astro.json';
@@ -9,21 +10,21 @@ export default class SsrUtil {
 	/**
 	 * SSR ページの初期処理
 	 *
-	 * @param name - サイト内でユニークなページの名前
+	 * @param Astro - `Astro` global <https://docs.astro.build/en/reference/api-reference/#astro-global>
 	 *
 	 * @returns 共通で使用されるデータ
 	 */
 	static init = async (
-		name: string,
+		Astro: AstroGlobal,
 	): Promise<{
 		configure: Configure;
 		logger: Log4js.Logger;
 	}> => {
-		/* Configure file */
+		Astro.response.headers.set('Content-Type', 'text/html;charset=utf-8');
 
 		/* Log4js */
 		Log4js.configure(log4jsConfigure);
-		const logger = Log4js.getLogger(name);
+		const logger = Log4js.getLogger(Astro.url.pathname);
 
 		return {
 			configure: configure,
