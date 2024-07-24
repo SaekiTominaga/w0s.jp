a7ad099bf8f8b06e5c71ba0210e3951a5ae4284a
SaekiTominaga <kcsu3470+github@gmail.com>
2024-07-24 19:37:01 +0900

    <portal> 要素削除

--- astro/script/error.ts
+++ astro/script/error.ts
@@ -1,5 +1,4 @@
 import ClosestHTMLPage from '@w0s/closest-html-page';
-import PortalAnimation from '@w0s/portal-animation';
 import ReportJsError from '@w0s/report-js-error';
 import ReportSameReferrer from '@w0s/report-same-referrer';
 
@@ -46,21 +45,12 @@ if (portalHost === null || portalHost === undefined /* <potal> 未対応ブラ�
 	if (url !== null && title !== null) {
 		const messageElement = document.getElementById('parentpage-msg');
 		const anchorElement = document.getElementById('parentpage-anchor') as HTMLAnchorElement | null;
-		const portalElement = document.getElementById('parentpage-portal') as HTMLPortalElement | null;
 
 		if (messageElement !== null && anchorElement !== null) {
 			messageElement.hidden = false;
 
 			anchorElement.href = url;
 			anchorElement.textContent = title;
-
-			if (portalElement !== null && window.HTMLPortalElement !== undefined /* <potal> 要素をサポートしているか */ && window.customElements !== undefined) {
-				portalElement.src = url;
-				portalElement.title = title;
-				portalElement.hidden = false;
-
-				customElements.define('w0s-portal', PortalAnimation);
-			}
 		}
 	}
 }
