51e32977e4f100d711e48162be6639a8b9ae8373
SaekiTominaga <kcsu3470+github@gmail.com>
2024-12-09 12:49:11 +0900

    脚注コンポーネントを注釈と文献に分離

--- astro/src/+util/Footnote.ts
+++ astro/src/+util/Footnote.ts
@@ -6,31 +6,69 @@ export default class FootnoteUtil {
 	 *
 	 * @returns 脚注データ
 	 */
-	static getData = (document: Document): Map<number, string | undefined> => {
-		const footnoteData = new Map<number, string | undefined>();
+	static getData = (document: Document): { note: Map<number, string | undefined>; reference: Map<number, string | undefined> } => {
+		const noteData = new Map<number, string | undefined>();
+		const referenceData = new Map<number, string | undefined>();
 
-		document.querySelectorAll<HTMLTemplateElement>('.astro-footnote-reference').forEach((footnoteReferenceTemplate, index) => {
+		document.querySelectorAll<HTMLTemplateElement>('.astro-noteref.-note').forEach((noterefTemplate, index) => {
 			const no = index + 1;
 
-			const templateElementClone = footnoteReferenceTemplate.content.cloneNode(true) as DocumentFragment;
+			const templateElementClone = noterefTemplate.content.cloneNode(true) as DocumentFragment;
 
-			const slotElement = templateElementClone.querySelector('.build-slot');
-			footnoteData.set(no, slotElement?.innerHTML);
-			slotElement?.remove();
+			const id = noterefTemplate.dataset.by;
+			if (id === undefined) {
+				throw new Error('`data-by` attribute is not set');
+			}
+
+			const contentElement = document.getElementById(id);
+
+			noteData.set(no, contentElement?.innerHTML);
+			contentElement?.remove();
 
 			const aElement = templateElementClone.querySelector('a');
 			if (aElement !== null) {
-				aElement.href = `#fn-${String(no)}`;
-				aElement.id = `fnref-${String(no)}`;
+				aElement.href = `#fn-note${String(no)}`;
+				aElement.id = `fnref-note${String(no)}`;
 			}
 
-			const numberElement = templateElementClone.querySelector('.build-number');
+			const numberElement = templateElementClone.querySelector('x-number');
 			numberElement?.replaceWith(document.createTextNode(String(no)));
 
-			footnoteReferenceTemplate.parentNode?.appendChild(templateElementClone);
-			footnoteReferenceTemplate.remove();
+			noterefTemplate.parentNode?.appendChild(templateElementClone);
+			noterefTemplate.remove();
 		});
 
-		return footnoteData;
+		document.querySelectorAll<HTMLTemplateElement>('.astro-noteref.-ref').forEach((noterefTemplate, index) => {
+			const no = index + 1;
+
+			const templateElementClone = noterefTemplate.content.cloneNode(true) as DocumentFragment;
+
+			const id = noterefTemplate.dataset.by;
+			if (id === undefined) {
+				throw new Error('`data-by` attribute is not set');
+			}
+
+			const contentElement = document.getElementById(id);
+
+			referenceData.set(no, contentElement?.innerHTML);
+			contentElement?.remove();
+
+			const aElement = templateElementClone.querySelector('a');
+			if (aElement !== null) {
+				aElement.href = `#fn-ref${String(no)}`;
+				aElement.id = `fnref-ref${String(no)}`;
+			}
+
+			const numberElement = templateElementClone.querySelector('x-number');
+			numberElement?.replaceWith(document.createTextNode(String(no)));
+
+			noterefTemplate.parentNode?.appendChild(templateElementClone);
+			noterefTemplate.remove();
+		});
+
+		return {
+			note: noteData,
+			reference: referenceData,
+		};
 	};
 }
