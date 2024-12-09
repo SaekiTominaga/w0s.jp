f744b0d833b4976e5493f129b50ca11af5ce64f0
SaekiTominaga <kcsu3470+github@gmail.com>
2024-12-09 13:27:57 +0900

    Util クラス変更

--- astro/src/+util/Footnote.ts
+++ astro/src/+util/Footnote.ts
@@ -1,74 +1,68 @@
-export default class FootnoteUtil {
-	/**
-	 * 脚注データを取得する
-	 *
-	 * @param document - <main> 要素
-	 *
-	 * @returns 脚注データ
-	 */
-	static getData = (document: Document): { note: Map<number, string | undefined>; reference: Map<number, string | undefined> } => {
-		const noteData = new Map<number, string | undefined>();
-		const referenceData = new Map<number, string | undefined>();
+const getData = (
+	document: Document,
+	options: {
+		selector: string;
+		noteRefIdPrefix: string;
+		footnoteIdPrefix: string;
+	},
+): Map<number, string | undefined> => {
+	const data = new Map<number, string | undefined>();
 
-		document.querySelectorAll<HTMLTemplateElement>('.astro-noteref.-note').forEach((noterefTemplate, index) => {
-			const no = index + 1;
+	document.querySelectorAll<HTMLTemplateElement>(options.selector).forEach((noterefTemplate, index) => {
+		const no = index + 1;
 
-			const templateElementClone = noterefTemplate.content.cloneNode(true) as DocumentFragment;
+		const templateElementClone = noterefTemplate.content.cloneNode(true) as DocumentFragment;
 
-			const id = noterefTemplate.dataset.by;
-			if (id === undefined) {
-				throw new Error('`data-by` attribute is not set');
-			}
+		const id = noterefTemplate.dataset.by;
+		if (id === undefined) {
+			throw new Error('`data-by` attribute is not set');
+		}
 
-			const contentElement = document.getElementById(id);
+		const contentElement = document.getElementById(id);
 
-			noteData.set(no, contentElement?.innerHTML);
-			contentElement?.remove();
+		data.set(no, contentElement?.innerHTML);
+		contentElement?.remove();
 
-			const aElement = templateElementClone.querySelector('a');
-			if (aElement !== null) {
-				aElement.href = `#fn-note${String(no)}`;
-				aElement.id = `fnref-note${String(no)}`;
-			}
+		const aElement = templateElementClone.querySelector('a');
+		if (aElement !== null) {
+			aElement.href = `#${options.footnoteIdPrefix}${String(no)}`;
+			aElement.id = `${options.noteRefIdPrefix}${String(no)}`;
+		}
 
-			const numberElement = templateElementClone.querySelector('x-number');
-			numberElement?.replaceWith(document.createTextNode(String(no)));
+		const numberElement = templateElementClone.querySelector('x-number');
+		numberElement?.replaceWith(document.createTextNode(String(no)));
 
-			noterefTemplate.parentNode?.appendChild(templateElementClone);
-			noterefTemplate.remove();
-		});
+		noterefTemplate.parentNode?.appendChild(templateElementClone);
+		noterefTemplate.remove();
+	});
 
-		document.querySelectorAll<HTMLTemplateElement>('.astro-noteref.-ref').forEach((noterefTemplate, index) => {
-			const no = index + 1;
+	return data;
+};
 
-			const templateElementClone = noterefTemplate.content.cloneNode(true) as DocumentFragment;
+/**
+ * 注釈データを取得する
+ *
+ * @param document - <main> 要素
+ *
+ * @returns 注釈データ
+ */
+export const getNoteData = (document: Document): Map<number, string | undefined> =>
+	getData(document, {
+		selector: '.astro-noteref.-note',
+		noteRefIdPrefix: 'nr-note',
+		footnoteIdPrefix: 'fn-note',
+	});
 
-			const id = noterefTemplate.dataset.by;
-			if (id === undefined) {
-				throw new Error('`data-by` attribute is not set');
-			}
-
-			const contentElement = document.getElementById(id);
-
-			referenceData.set(no, contentElement?.innerHTML);
-			contentElement?.remove();
-
-			const aElement = templateElementClone.querySelector('a');
-			if (aElement !== null) {
-				aElement.href = `#fn-ref${String(no)}`;
-				aElement.id = `fnref-ref${String(no)}`;
-			}
-
-			const numberElement = templateElementClone.querySelector('x-number');
-			numberElement?.replaceWith(document.createTextNode(String(no)));
-
-			noterefTemplate.parentNode?.appendChild(templateElementClone);
-			noterefTemplate.remove();
-		});
-
-		return {
-			note: noteData,
-			reference: referenceData,
-		};
-	};
-}
+/**
+ * 参考文献データを取得する
+ *
+ * @param document - <main> 要素
+ *
+ * @returns 参考文献データ
+ */
+export const getReferenceData = (document: Document): Map<number, string | undefined> =>
+	getData(document, {
+		selector: '.astro-noteref.-ref',
+		noteRefIdPrefix: 'nr-ref',
+		footnoteIdPrefix: 'fn-ref',
+	});
