0ea5c262d7a592482898600f3ca28f0bc46966a4
SaekiTominaga <kcsu3470+github@gmail.com>
2025-05-11 17:44:11 +0900

    自動 ID 生成処理に base64url をかける

---
+++ astro/src/+util/crypto.ts
@@ -0,0 +1,20 @@
+import crypto from 'node:crypto';
+
+const base64UrlEncode = (str: string): string => {
+	const base64 = btoa(str);
+	return base64.replaceAll('+', '-').replaceAll('/', '_').replace(/=+$/, '');
+};
+
+export const getId = (str?: string): string => {
+	if (str !== undefined) {
+		const hash = crypto.createHash('md5');
+		hash.update(str);
+
+		return base64UrlEncode(hash.digest('hex'));
+	}
+
+	const array = new Uint8Array(16);
+	crypto.getRandomValues(array);
+
+	return base64UrlEncode(String.fromCharCode(...array));
+};
--- astro/src/+util/hash.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'node:crypto';
-
-export const getIdHash = (str: string): string => {
-	const hash = crypto.createHash('md5');
-	hash.update(str);
-	return hash.digest('hex');
-};
