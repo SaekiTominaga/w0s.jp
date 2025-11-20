c504185c9b27792da74878087f002d56dafe2a35
SaekiTominaga <kcsu3470+github@gmail.com>
2025-11-20 14:21:24 +0900

    kysely 導入

---
+++ @types/db_tokyuhistory.d.ts
@@ -0,0 +1,67 @@
+import type { Generated } from 'kysely';
+
+type TypeTransform<T> = T extends boolean ? 0 | 1 : T extends Date ? number : T extends URL ? string : T;
+type NullTransform<T> = Exclude<T, undefined> | (undefined extends T ? null : never);
+type Transform<T> = TypeTransform<NullTransform<T>>;
+
+export interface DCar {
+	num: string;
+	type: string;
+	annual: number;
+	annual_display: string | undefined;
+	register_date: number; // YYYYMMDD
+	renewal: number;
+	scrap: boolean;
+	transfer: string | undefined;
+	note: string | undefined;
+}
+
+export interface DChange {
+	now_num: string;
+	before_num: string;
+	before_type: string;
+	change_date: number; // YYYYMMDD
+}
+
+export interface MRenewal {
+	fk: number;
+	name: string;
+}
+
+export interface MSeries {
+	fk: string;
+	name: string;
+	display: number;
+	display_cargroup: number;
+	first_manufacture: number; // YYYY
+	company: number;
+	retire: number;
+}
+
+export interface MSign {
+	fk: string;
+	name: string;
+	sort: number;
+}
+
+export interface MType {
+	fk: string;
+	name: string;
+	sign: string;
+	series: string;
+}
+
+export interface VAnnual {
+	num: string | undefined;
+	annual: string | undefined;
+}
+
+export interface DB {
+	d_car: { [K in keyof DCar]: Transform<DCar[K]> };
+	d_change: { [K in keyof DChange]: Transform<DChange[K]> };
+	m_renewal: { [K in keyof MRenewal]: Transform<MRenewal[K]> };
+	m_series: { [K in keyof MSeries]: Transform<MSeries[K]> };
+	m_sign: { [K in keyof MSign]: Transform<MSign[K]> };
+	m_type: { [K in keyof MType]: Transform<MType[K]> };
+	v_annual: { [K in keyof VAnnual]: Transform<VAnnual[K]> };
+}
---
+++ astro/src/+db/DatabaseCrawler.ts
@@ -0,0 +1,62 @@
+import SQLite from 'better-sqlite3';
+import { Kysely, SqliteDialect } from 'kysely';
+import { sqliteToJS } from '@w0s/sqlite-utility';
+import type { DB, MCategory, MPriority } from '../../../@types/db_crawler';
+
+/**
+ * ウェブ巡回
+ */
+export default class CrawlerDao {
+	protected readonly db: Kysely<DB>;
+
+	/**
+	 * @param filePath - DB ファイルパス
+	 * @param options - オプション
+	 */
+	constructor(filePath: string, options?: Readonly<Pick<SQLite.Options, 'readonly'>>) {
+		const sqlite = new SQLite(filePath, {
+			/* https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md#new-databasepath-options */
+			readonly: options?.readonly ?? false,
+			fileMustExist: true,
+		});
+		sqlite.pragma('journal_mode = WAL');
+
+		this.db = new Kysely<DB>({
+			dialect: new SqliteDialect({
+				database: sqlite,
+			}),
+		});
+	}
+
+	/**
+	 * カテゴリー情報を取得
+	 *
+	 * @returns カテゴリー情報
+	 */
+	async getCategoryMaster(): Promise<Pick<MCategory, 'fk' | 'name'>[]> {
+		const query = this.db.selectFrom('m_category').select(['fk', 'name']).orderBy('sort');
+
+		const rows = await query.execute();
+
+		return rows.map((row) => ({
+			fk: sqliteToJS(row.fk),
+			name: sqliteToJS(row.name),
+		}));
+	}
+
+	/**
+	 * 優先度情報を取得
+	 *
+	 * @returns 優先度情報
+	 */
+	async getPriorityMaster(): Promise<MPriority[]> {
+		const query = this.db.selectFrom('m_priority').select(['fk', 'name']).orderBy('fk');
+
+		const rows = await query.execute();
+
+		return rows.map((row) => ({
+			fk: sqliteToJS(row.fk),
+			name: sqliteToJS(row.name),
+		}));
+	}
+}
