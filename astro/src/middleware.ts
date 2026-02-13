3288c3c1fb20459f05e4ab9dceae12c3a50ed804
SaekiTominaga <kcsu3470+github@gmail.com>
2026-02-13 18:45:59 +0900

    SSG ページでは `<link rel="stylesheet"> の位置をまとめるようにする

--- astro/src/middleware.ts
+++ astro/src/middleware.ts
@@ -1,21 +1,38 @@
 import { loadEnvFile } from 'node:process';
 import { defineMiddleware } from 'astro:middleware';
+import * as cheerio from 'cheerio';
 import Log4js from 'log4js';
 import { env } from '@w0s/env-value-type';
 import URLSearchParamsCustomSeparator from '@w0s/urlsearchparams-custom-separator';
 
 export const onRequest = defineMiddleware(async (context, next) => {
-	const { status, statusText, headers, body } = await next();
+	const response = await next();
+	const { status, statusText, headers, body } = response;
 
 	const contentType = headers.get('Content-Type');
 	if (contentType !== null && ['text/html'].includes(contentType)) {
 		headers.set('Content-Type', `${contentType};charset=utf-8`);
 	}
 
-	console.debug(import.meta);
+	if (context.isPrerendered) {
+		/* SSG */
+		if (!import.meta.env.DEV) {
+			const $ = cheerio.load(await response.text());
 
-	/* SSR ページ */
-	if (!context.isPrerendered) {
+			/* `<link rel="stylesheet"> の位置をまとめる` */
+			const $linkStylesheets = $('head > link[rel~="stylesheet"]');
+			if ($linkStylesheets.length >= 2) {
+				const $astroStylesheet = $linkStylesheets.last();
+				$astroStylesheet.insertAfter($linkStylesheets.eq(-2));
+			}
+
+			return new Response($.html(), {
+				status: status,
+				statusText: statusText,
+				headers: headers,
+			});
+		}
+	} else {
 		loadEnvFile(!import.meta.env.DEV ? '../.env.production' : '../.env.development');
 
 		/* Logger */
