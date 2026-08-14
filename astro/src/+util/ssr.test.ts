d9f4ee31cf8a4f5f7eb7c3c8890ea7e379cf496e
SaekiTominaga <kcsu3470+github@gmail.com>
2026-08-14 22:17:51 +0900

    build 時に Astro.url.pathname に拡張子が含まれるようになったことに伴う修正

--- astro/src/+util/ssr.test.ts
+++ astro/src/+util/ssr.test.ts
@@ -3,14 +3,8 @@ import { test } from 'node:test';
 import { getPageUrl } from './ssr.ts';
 
 await test('getPageUrl', async (t) => {
-	await t.test('normal page', async (t2) => {
-		await t2.test('dev', () => {
-			assert.equal(getPageUrl(new URL('http://example.com/path/to'), '/var/www/path/to.astro'), '/path/to');
-		});
-
-		await t2.test('build', () => {
-			assert.equal(getPageUrl(new URL('http://example.com/path/to/'), '/var/www/path/to.astro'), '/path/to');
-		});
+	await t.test('top page', () => {
+		assert.equal(getPageUrl(new URL('http://example.com/'), '/var/www/index.astro'), '/');
 	});
 
 	await t.test('index page', async (t2) => {
@@ -20,6 +14,16 @@ await test('getPageUrl', async (t) => {
 
 		await t2.test('build', () => {
 			assert.equal(getPageUrl(new URL('http://example.com/path/to/'), '/var/www/path/to/index.astro'), '/path/to/');
+		});
+	});
+
+	await t.test('normal page', async (t2) => {
+		await t2.test('dev', () => {
+			assert.equal(getPageUrl(new URL('http://example.com/path/to'), '/var/www/path/to.astro'), '/path/to');
+		});
+
+		await t2.test('build', () => {
+			assert.equal(getPageUrl(new URL('http://example.com/path/to.html'), '/var/www/path/to.astro'), '/path/to');
 		});
 	});
 });
